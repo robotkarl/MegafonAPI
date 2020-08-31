e394d6d4ccda64e4e6a6909a10d67fd49adf90af
Ilya Strukov <ilya@strukov.net>
2020-08-31 22:00:31 +0800

    Dependencies updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="MegafonAPI-robotkarl",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Ilya Strukov",
     author_email="ilya@strukov.net",
     description="Python API implementation to work with Megafon business services",
